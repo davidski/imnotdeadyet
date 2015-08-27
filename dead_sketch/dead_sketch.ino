ca4e4a1c6861914190b50bfc1e307d9d44e46263
davidski <davidski@deadheaven.com>
2015-08-26 20:03:40 -0700

    Initial commit

---
+++ dead_sketch/dead_sketch.ino
@@ -0,0 +1,18 @@
+#include <Bridge.h>
+#include <Console.h>
+#include <FileIO.h>
+#include <HttpClient.h>
+#include <Mailbox.h>
+#include <Process.h>
+#include <YunClient.h>
+#include <YunServer.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}

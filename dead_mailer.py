9a527d999075a92de3db174e0696e961c05041c4
davidski <davidski@deadheaven.com>
2015-10-26 20:29:22 -0700

    Break down into core functions

--- dead_mailer.py
+++ dead_mailer.py
@@ -1,21 +1,57 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import boto3
+from random import sample
 
-client = boto3.client('ses')
+def acknowledge_send():
+	print "Acknowledge"
 
-client.send_email(
-	Source='david@severski.net',
-	Message={
-		'Subject': {
-			'Data': 'Here!',
-		},
-		'Body': {
-			'Text': {
-				'Data': "I'm not dead yet!",
+
+def indicate_failure():
+	print "Failure"
+
+
+def set_message_body(selection):
+	switcher = {
+		'0': "I'm here!",
+		'1': "Brrrraaaaains!",
+		'2': "Arrived!"
+	}
+	return switcher.get(str(selection), 'nothing')
+
+
+if __name__ == "__main__":
+
+	client = boto3.client('ses')
+
+	# read input
+	selection  = sample([0, 1, 2], 1)[0]
+	#selection = 1
+
+	# set message body
+	message_body = set_message_body(selection)
+
+	# send email
+	response = client.send_email(
+		Source='david@severski.net',
+		Message={
+			'Subject': {
+				'Data': 'Here!',
+			},
+			'Body': {
+				'Text': {
+					'Data': message_body,
+				}
 			}
-		}
-	},
-	Destination={'ToAddresses': ['davidski@deadheaven.com']}
-)
+		},
+		Destination={'ToAddresses': ['davidski@deadheaven.com']}
+	)
 
+	# check response
+	if 'MessageId' in response:
+		print('Sent, with a message ID of {}'.format(response['MessageId']))
+		acknowledge_send()
+	else:
+		print('Could not find a valid response')
+		print response
+		indicate_failure()
